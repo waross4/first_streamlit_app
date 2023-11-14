725d8493bab5ed430730cba1b3afd2259c6918a5
waross4 <42983949+waross4@users.noreply.github.com>
2023-11-14 15:08:38 -0700

    Update streamlit_app.py
    Adding user functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,12 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+##Creating the data set for fruit
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+##Displaying the data set for fruit
 streamlit.dataframe(my_fruit_list)
+
+##Asking for user input (selection) for fruit
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
