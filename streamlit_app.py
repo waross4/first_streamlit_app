b467fba516a2fe38ebab413153c848d1ae290a7b
waross4 <42983949+waross4@users.noreply.github.com>
2023-11-14 15:14:15 -0700

    Update streamlit_app.py
    Dataframe updates to user choice (includes default)

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,8 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 ##Asking for user input (selection) for fruit
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 ##Displaying the data set for fruit
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
